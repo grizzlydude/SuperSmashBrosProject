56b3716f17a72675e334170681f5efd6e133e8d1
Grant <gwensignak@gmail.com>
2019-07-18 12:26:51 -0600

    inital commit

---
+++ src/SmashData.js
@@ -0,0 +1,92 @@
+const SmashData = [
+    {
+        id: 1,
+        name: 'Mario',
+        difficulty: 'Beginner',
+        image: ''
+    },
+    {
+        id: 2,
+        name: 'Samus',
+        difficulty: 'Beginner',
+        image: ''
+    },
+    {
+        id: 3,
+        name: 'Peach',
+        difficulty: 'Beginner',
+        image: ''
+    },
+    {
+        id: 4,
+        name: 'Ness',
+        difficulty: 'Experienced',
+        image: ''
+    },
+    {
+        id: 5,
+        name: 'Pickachu',
+        difficulty: 'Beginner',
+        image: ''
+    },
+    {
+        id: 6,
+        name: 'Link',
+        difficulty: 'Beginner',
+        image: ''
+    },
+    {
+        id: 7,
+        name: 'Zelda',
+        difficulty: 'Intermediate',
+        image: ''
+    },
+    {
+        id: 8,
+        name: 'Joker',
+        difficulty: 'Experienced',
+        image: ''
+    },
+    {
+        id: 9,
+        name: 'Bowser',
+        difficulty: 'Intermediate',
+        image: ''
+    },
+    {
+        id: 10,
+        name: 'Donkey-Kong',
+        difficulty: 'Beginner',
+        image: ''
+    },
+    {
+        id: 11,
+        name: 'Wii Fit Trainer',
+        difficulty: 'Intermediate',
+        image: ''
+    },
+    {
+        id: 12,
+        name: 'Ganadorf',
+        difficulty: 'Intermediate',
+        image: ''
+    },
+    {
+        id: 13,
+        name: 'Zero-Suit Smaus',
+        difficulty: 'Intermediate',
+        image: ''
+    },
+    {
+        id: 14,
+        name: 'Snake',
+        difficulty: 'Experienced',
+        image: ''
+    },
+    {
+        id: 15,
+        name: 'Banjo-Kazooie',
+        difficulty: 'unknown',
+        image:''
+    }
+]
